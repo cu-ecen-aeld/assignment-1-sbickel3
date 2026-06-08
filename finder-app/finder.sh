4be6d267479d88c8bcde4bcdfad5d9c838a9e566
Shawn Bickel <sbickel201@gmail.com>
2026-06-08 00:23:30 -0400

    hiding file names

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -11,7 +11,7 @@ searchstr="$2"
 
 if [ -d "$filesdir" ]; then
     numberOfFiles=$( find "$filesdir" -type f 2>/dev/null | wc -l )
-    numberOfMatches=$( grep -r "$searchstr" "$filesdir" 2>/dev/null | wc -l )
+    numberOfMatches=$( grep -rh "$searchstr" "$filesdir" 2>/dev/null | wc -l )
     echo "The number of files are $numberOfFiles and the number of matching lines are $numberOfMatches"
 else
     echo "Error: '$filesdir' does not exist or is not a directory."
